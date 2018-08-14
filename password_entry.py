e423864625c090d93cc6de90d5c5fabecbd2dab1
jc496578 <cassandra.king4@my.jcu.edu.au>
2018-08-14 14:55:56 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,3 +1,9 @@
-"""Cassandra King"""
-#do nothing
-
+password = input("Password Please: ")
+while len(password) < 3:
+    print("Too Small")
+    password = input("Password Please: ")
+if len(password) > 10:
+    print("Too Large")
+    password = input("Password Please: ")
+print( '*' * len(password))
+print()
